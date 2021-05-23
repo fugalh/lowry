6506d337768475743a9c0a20325db3b5c0bc2c7b
Hans Fugal <hans@fb.com>
2021-05-22 17:22:10 -0700

    data plate: easy
    C_D0, e, m, and b are harder.
    
    C should probably be an optional input but I'm not sure how anyone would
    calculate/measure/estimate it so let's leave it constant for now.

--- js/lowry.js
+++ js/lowry.js
@@ -1,7 +1,10 @@
 const math = require('mathjs')
 
 // custom units
-math.createUnit('slug', '1 lbf s^2 / ft');
+math.createUnit('slug', '1 lbf s^2 / ft')
+math.createUnit('HP', '550 ft lbf / sec')
+math.createUnit('rpm', '1/minute')
+math.createUnit('rps', '1/second')
 
 // constants
 const constants = {
@@ -9,4 +12,16 @@ const constants = {
     rho0: math.unit('0.00237 slug/ft^3'), // standard density
 }
 
-exports.constants = constants;
+exports.plate = (data) => {
+    const tau = 2 * math.pi;
+    return {
+        S: data.S,
+        A: math.divide(math.multiply(data.B, data.B), data.S),
+        M0: math.divide(data.P0, math.multiply(tau, data.n0)),
+        C: constants.C,
+        d: data.d,
+    };
+}
+
+exports.constants = constants
+exports.math = math
--- js/lowry.test.js
+++ js/lowry.test.js
@@ -1,6 +1,27 @@
 const lowry = require('./lowry')
-c = lowry.constants;
+const math = lowry.math
+const c = lowry.constants
+
+const tau = 2 * math.pi;
+
+data = {
+    S: math.unit('174 ft^2'),
+    B: math.unit('35.83 ft'),
+    P0: math.unit('160 HP'),
+    n0: math.unit('2700 rpm'),
+    d: math.unit('6.25 ft'),
+    W0: math.unit('2400 lbf'),
+}
 
 test('I know how to program', () => {
-    expect(c.rho0.toNumber('kg/m^3')).toBeCloseTo(1.2214478)
+    expect(c.rho0.toNumber('kg/m^3')).toBeCloseTo(1.2214478);
+})
+
+test('data plate', () => {
+    plate = lowry.plate(data);
+    expect(plate.S.toNumber('ft^2')).toBe(174);
+    expect(plate.A).toBeCloseTo(7.378,0.001);
+    expect(plate.M0.toNumber('ft lbf')).toBeCloseTo(311.2, 0.1);
+    expect(plate.C).toBe(0.12);
+    expect(plate.d.toNumber('ft')).toBe(6.25);
 })
