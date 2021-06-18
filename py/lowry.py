b7fde614a1bfba92befd11abf90e7476dee1d780
Hans Fugal <hans@fb.com>
2021-06-18 14:49:13 -0700

    ported bootstrap code and appendix F test

--- py/lowry.py
+++ py/lowry.py
@@ -74,9 +74,61 @@ def flightAngle(V, dh, dt):
     return Q_(math.asin(dh / (V * dt)), 'radian').to('degree')
 
 # The Bootstrap Method
-def bootstrap(input):
-    """ airframe and flight test input -> bootstrap data plate """
-    pass
+def bootstrap(data):
+    """ airframe and flight test data -> bootstrap data plate """
+    plate = data  # TODO extract only the canonical?
+    if 'A' not in data and 'B' in data and 'S' in data:
+        plate['A'] = data['B'] ** 2 / data['S']
+    if 'M0' not in data and 'P0' in data and 'n0' in data:
+        plate['M0'] = data['P0'] / (2 * math.pi * data['n0'])
+    if 'C' not in data:
+        plate['C'] = 0.12
+
+    if 'drag' in data:
+        # [PoLA] appendix F
+        drag = data['drag']
+        h_p = drag['h_p']
+        T = drag['T']
+        sigma = relativeDensity(h_p, T)
+        dh = tapeline(drag['dh_p'], h_p, T)
+        Vbg = tas(drag['VCbg'], h_p, T)
+        gamma_bg = flightAngle(Vbg, dh, drag['dt'])
+
+        # [PoLA] eq 9.41
+        # I think there's a sign error; eq 9.41 uses -W but that gives the wrong sign.
+        rho = rho0 * sigma
+        plate['C_D0'] = drag['W'] * math.sin(gamma_bg) / (rho * plate['S'] * Vbg ** 2)
+        plate['e'] = 4 * plate['C_D0'] / (math.pi * plate['A'] * math.tan(gamma_bg) ** 2)
+
+    if 'thrust' in data:
+        thrust = data['thrust']
+        h_p = thrust['h_p']
+        T = thrust['T']
+        rho = density(thrust['h_p'], thrust['T'])
+        phi = dropoffFactor(h_p, T)
+        d = thrust['d']
+        Vx = tas(thrust['VCx'], h_p, T)
+        V_M = tas(thrust['V_CM'], h_p, T)
+
+        # [Bootstrap] eq 8, [PoLA] eq 7.1
+        plate['b'] = plate['S'] * plate['C_D0'] / (2 * d * d) - (
+            2 * W * W / (rho * rho * d * d * plate['S']
+                * math.pi * plate['e'] * plate['A'] * Vx ** 4)
+        )
+
+
+        # [Bootstrap] eq 9, but substituting πM0 = P0/2n0
+        plate['m'] = (d * W * W /
+            (math.pi * plate['M0'] * phi * rho * plate['S'] *
+                math.pi * plate['e'] * plate['A'])
+        ) * (1 / (V_M * V_M) + (V_M * V_M) / (Vx ** 4))
+
+    # mock overrides for testing
+    for k in ['C_D0', 'e', 'b', 'm']:
+        if k in data:
+            plate[k] = data[k]
+
+    return plate
 
 
 def performance(plate, W, h_rho, V = None):
--- py/test/test_lowry.py
+++ py/test/test_lowry.py
@@ -55,3 +55,51 @@ class TestAtmosphere:
 
         assert_approx_Q(tapeline, Q_(506.5, 'ft'), abs=0.1)
         assert_approx_Q(gamma, Q_(6.21, 'deg'), abs=0.01)
+
+class TestBootstrap:
+    def test_appendixF(self):
+        data = {
+            'S': Q_('174 ft^2'),
+            'B': Q_('35.83 ft'),
+            'P0': Q_('160 horsepower'),
+            'n0': Q_('2700 rpm'),
+            # C: 0.12,
+            'd': Q_('6.25 ft'),
+            'drag': {
+                'W': Q_('2200 lbf'),
+                'dh_p': Q_('200 ft'),
+                'VCbg': Q_('70 kts'),
+                'dt': Q_('17.0 sec'),
+
+                'W': Q_('2209 lbf'),
+                'h_p': Q_('5750 ft'),
+                'T': Q_(45, 'degF'),
+                'VCbg': Q_('70.5 kts'),
+                'dh_p': Q_('500 ft'),
+                'dt': Q_('39.10 s'),
+            }
+        }
+
+        drag = data['drag']
+        VCbg = drag['VCbg']
+        dt = drag['dt']
+        h_p = drag['h_p']
+        T = drag['T']
+
+        assert_approx_Q(VCbg, Q_(119.0, 'ft/s'), abs=0.1)
+        assert lowry.relativeDensity(h_p, T) == approx(0.9871, abs=1e-4)
+        assert_approx_Q(lowry.density(h_p, T), Q_(0.002339, 'slug / ft^3'), rel=1e-3)
+
+        Vbg = lowry.tas(VCbg, h_p, T)
+        assert_approx_Q(Vbg, Q_(119.8, 'ft/s'), abs=0.1)
+
+        dh_p = Q_('500 ft')
+        dh = lowry.tapeline(dh_p, h_p, T)
+        assert_approx_Q(dh, Q_('506.5 ft'), abs=0.1)
+        assert_approx_Q(lowry.flightAngle(Vbg, dh, dt), Q_(6.21, 'deg'), abs=0.01)
+
+        plate = lowry.bootstrap(data)
+
+        # hand calculated, slightly different from the book
+        assert plate['C_D0'] == approx(0.04093, rel=1e-3)
+        assert plate['e'] == approx(0.5964, rel=1e-3)
